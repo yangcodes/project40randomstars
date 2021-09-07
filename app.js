35ba6753eba50b6c3ad0ee266320cd1c78f9d801
Jenny Lin <misslinyang91@gmail.com>
2021-09-07 14:18:41 +1000

    created velocity and randomized star size and location

--- app.js
+++ app.js
@@ -10,14 +10,40 @@ const size = 0.005; // maximum star size as a fraction of the screen width
 const speed = 0.05; // fraction of screen width per second
 
 // Setting up the stars
+let stars = [];
+let starsSpeed = speed * canvasEl.width;
+let horizontalVelocity = starsSpeed * randomSign() * Math.random();
+let verticalVelocity =
+  Math.sqrt(Math.pow(starsSpeed, 2) - Math.pow(horizontalVelocity, 2)) *
+  randomSign();
+
+//randomizing the stars speed, size and location
+for (let i = 0; i < starsNumber; i++) {
+  let speedBoost = Math.random() * 2.5 + 0.5;
+  stars[i] = {
+    starRadius: (Math.random() * size * canvasEl.width) / 2,
+    horizontalPosition: Math.floor(Math.random() * canvasEl.width),
+    verticalPosition: Math.floor(Math.random() * canvasEl.height),
+    horizontalVelocity: horizontalVelocity * speedBoost,
+    verticalVelocity: verticalVelocity * speedBoost,
+  };
+}
 
 //--------------------- The Game Loop
-function runStars() {
+
+//the animation loop
+let timeDiff,
+  timeLast = 0;
+requestAnimationFrame(runStars);
+
+function runStars(timeNow) {
+  timeDiff = timeNow - timeLast;
+  timeLast = timeNow;
   console.log("running");
   requestAnimationFrame(runStars);
 }
 
-// randomSign Function
-// function randomSign() {
-//   return Math.random() >= 0.5 ? 1 : -1;
-// }
+//randomSign Function
+function randomSign() {
+  return Math.random() >= 0.5 ? 1 : -1;
+}
